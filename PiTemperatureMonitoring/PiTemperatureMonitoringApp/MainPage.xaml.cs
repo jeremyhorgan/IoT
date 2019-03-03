88f87be1e4f4ce39f89f0cdbc8d68f571a509f9c
Jeremy Horgan <jeremyhorgan@hotmail.com>
2019-03-03 21:51:04 +0000

    Update C# vesion to use DHT22

--- PiTemperatureMonitoring/PiTemperatureMonitoringApp/MainPage.xaml.cs
+++ PiTemperatureMonitoring/PiTemperatureMonitoringApp/MainPage.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
 using Windows.System.Threading;
 using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
 #if USE_PI
-    using Windows.Devices.Gpio;
-    using Sensors.Dht;
+using Windows.Devices.Gpio;
+using Sensors.Dht;
 #endif
 
 
@@ -19,14 +20,14 @@ namespace PiTemperatureMonitoringApp
     /// </remarks>
     public sealed partial class MainPage : Page
     {
-        private const int TemperatureMonitorIntervalInSeconds = 1;
+        private const int TemperatureMonitorIntervalInSeconds = 3;
 
         private ThreadPoolTimer _threadPoolTimer;
         private double _temperatureMax = double.MinValue;
         private double _temperatureMin = double.MaxValue;
         private double _temperatureNow;
 #if USE_PI
-        private Dht11 _dht11;
+        private Dht22 _sensor;
 #endif
         public MainPage()
         {
@@ -38,8 +39,10 @@ namespace PiTemperatureMonitoringApp
         private void InitializeSensor()
         {
 #if USE_PI
-            var gpioPin4 = GpioController.GetDefault().OpenPin(4, GpioSharingMode.Exclusive);
-            _dht11 = new Dht11(gpioPin4, GpioPinDriveMode.Input);
+            var dataPin = GpioController.GetDefault().OpenPin(4, GpioSharingMode.Exclusive);
+            dataPin.SetDriveMode(GpioPinDriveMode.Input);
+
+            _sensor = new Dht22(dataPin, GpioPinDriveMode.Input);
 #endif
         }
 
@@ -47,21 +50,29 @@ namespace PiTemperatureMonitoringApp
         {
             _threadPoolTimer = ThreadPoolTimer.CreatePeriodicTimer(async source =>
             {
+                bool isValid;
 #if USE_PI
-                var temperatureReading = await _dht11.GetReadingAsync();
+                var temperatureReading = await _sensor.GetReadingAsync();
+                isValid = temperatureReading.IsValid;
                 _temperatureNow = temperatureReading.Temperature;
 #else
                 _temperatureNow = new Random().NextDouble() * (30 - 18) + 18;
+                isValid = true;
 #endif
                 _temperatureMax = Math.Max(_temperatureMax, _temperatureNow);
                 _temperatureMin = Math.Min(_temperatureMin, _temperatureNow);
 
-                await Dispatcher.RunAsync(CoreDispatcherPriority.High, () =>
+                Debug.WriteLine($"Temperature: {_temperatureNow} isValid: {isValid}");
+
+                if (isValid)
                 {
-                    TemperatureNow.Text = $"Now: {_temperatureNow:#.##}° C";
-                    TemperatureMax.Text = $"High: {_temperatureMax:#.##}°";
-                    TemperatureMin.Text = $"Low: {_temperatureMin:#.##}°";
-                });
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.High, () =>
+                    {
+                        TemperatureNow.Text = $"Now: {_temperatureNow:#.##}° C";
+                        TemperatureMax.Text = $"High: {_temperatureMax:#.##}°";
+                        TemperatureMin.Text = $"Low: {_temperatureMin:#.##}°";
+                    });
+                }
 
             }, TimeSpan.FromSeconds(TemperatureMonitorIntervalInSeconds));
         }
